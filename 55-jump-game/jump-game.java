4378edf5b5fbc9f31d42ade469ccd4e44fb48728
Ayush Parashar <ayushparashar1419@gmail.com>
2026-06-19 07:33:51 +0530

    Time: 2 ms (88.77%) | Memory: 48.1 MB (23.09%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int mx = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > mx) {
+                return false;
+            }
+
+            mx = Math.max(mx, i + nums[i]);
+
+            if (mx >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
